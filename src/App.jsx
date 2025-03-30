26cd0db0114b643cd9d5d739014e107a9b788502
Scavenger233 <liyiminginireland@gmail.com>
2025-03-30 13:55:53 +0100

    set up the frontend framework with Tailwind CSS and React

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+function App() {
+    return (
+      <div className="text-3xl font-bold underline text-blue-500">
+        Tailwind working successfully！
+      </div>
+    );
+  }
+
+export default App;  // Make sure App is export by default
